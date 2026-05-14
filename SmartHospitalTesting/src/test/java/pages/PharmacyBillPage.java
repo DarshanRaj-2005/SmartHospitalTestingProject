780ee7f41eeef862c63957508617705e32afcb56
Balamurugan <murugan020325@gmail.com>
2026-05-14 22:39:11 +0530

    modified PharmacyBillPage

--- SmartHospitalTesting/src/test/java/pages/PharmacyBillPage.java
+++ SmartHospitalTesting/src/test/java/pages/PharmacyBillPage.java
@@ -3,12 +3,6 @@ package pages;
 import org.openqa.selenium.By;
 
 public class PharmacyBillPage {
-
-    // Pharmacy menu
-    public static By pharmacyMenu =
-            By.xpath("//span[normalize-space()='Pharmacy']");
-
-    // Pharmacy Bill page heading
-    public static By pharmacyBillPageHeader =
-            By.xpath("//h3[contains(text(),'Pharmacy Bill')]");
+    public static By pharmacyMenu = By.xpath("//span[normalize-space()='Pharmacy']");
+    public static By pharmacyBillPageHeader = By.xpath("//h3[contains(text(),'Pharmacy Bill')]");
 }
