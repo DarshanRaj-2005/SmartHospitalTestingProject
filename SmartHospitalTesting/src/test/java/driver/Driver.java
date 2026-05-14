b0c0a7c29b9bf01545739a38a6ed783c0a3ac947
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-05-14 11:46:47 +0530

    Project Structure Created

---
+++ SmartHospitalTesting/src/test/java/driver/Driver.java
@@ -0,0 +1,69 @@
+package driver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import Utilities.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+
+public class Driver {
+
+	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+	public static WebDriver getDriver() {
+
+		return driver.get();
+	}
+
+	public Driver() {
+
+		    String browser = ConfigReader.getProperties().getProperty("browser");
+		    String headlessValue = ConfigReader.getProperties().getProperty("headless");
+
+		    boolean headless = headlessValue != null && headlessValue.equalsIgnoreCase("true");
+
+		    if (browser.equalsIgnoreCase("chrome")) {
+
+		        WebDriverManager.chromedriver().setup();
+		        ChromeOptions options = new ChromeOptions();
+
+		        if (headless) {
+		            options.addArguments("--headless=new");
+		        }
+
+		        driver.set(new ChromeDriver(options));
+
+		    } else if (browser.equalsIgnoreCase("firefox")) {
+
+		        WebDriverManager.firefoxdriver().setup();
+		        FirefoxOptions options = new FirefoxOptions();
+
+		        if (headless) {
+		            options.addArguments("--headless=new");
+		        }
+
+		        driver.set(new FirefoxDriver(options));
+
+		    } else {
+		       
+		        throw new RuntimeException("Invalid Browser Name: " + browser);
+		    }
+
+		    getDriver().manage().window().maximize();
+		    
+		}
+
+	public static void quitDriver() {
+		if (getDriver() != null) {
+			getDriver().quit();
+			driver.remove();
+		}
+	}
+
+}
