0538c0c2484702d5331a9f2e8b48a3af69838fff
Balamurugan <murugan020325@gmail.com>
2026-05-14 22:37:33 +0530

    Update PharmacyBillpageDefinition.java

--- SmartHospitalTesting/src/test/java/stepDefinition/PharmacyBillpageDefinition.java
+++ SmartHospitalTesting/src/test/java/stepDefinition/PharmacyBillpageDefinition.java
@@ -10,8 +10,7 @@ import pages.PharmacyBillPage;
 
 public class PharmacyBillpageDefinition {
 
-    PharmacyBillpageAction pharmacyAction =
-            new PharmacyBillpageAction();
+    PharmacyBillpageAction pharmacyAction =new PharmacyBillpageAction();
 
     @When("the user clicks the Pharmacy")
     public void the_user_clicks_the_pharmacy() {
@@ -22,12 +21,8 @@ public class PharmacyBillpageDefinition {
     @Then("it should move to the Pharmacy Bill page successfully")
     public void it_should_move_to_the_pharmacy_bill_page_successfully() {
 
-        boolean isDisplayed =
-                Driver.getDriver()
-                .findElement(PharmacyBillPage.pharmacyBillPageHeader)
-                .isDisplayed();
-
-        Assert.assertTrue(isDisplayed);
-        System.out.println("successfully moved to the pharmacy bill page");
+      boolean isDisplayed = Driver.getDriver().findElement(PharmacyBillPage.pharmacyBillPageHeader).isDisplayed();
+      Assert.assertTrue(isDisplayed);
+	  System.out.println("successfully moved to the pharmacy bill page");
     }
 }
