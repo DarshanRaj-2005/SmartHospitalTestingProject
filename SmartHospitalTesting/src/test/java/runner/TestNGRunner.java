700b3fb508be2b683a82d92d644e4a1fafda6bf8
Harini R G <harinirg26@gmail.com>
2026-05-15 07:43:23 +0530

    TestNGRunner is committed.

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features = {
                
-                "src/test/resources/Tamilarasu/AddNewStock.feature"
+                "src\\test\\resources\\feature_files\\darshan_raj\\login.feature"
         },
 
         glue = {"stepDefinition", "hooks"},
