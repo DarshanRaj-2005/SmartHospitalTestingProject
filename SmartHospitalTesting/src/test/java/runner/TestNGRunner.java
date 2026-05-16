77d795de318257f664124ee769aae514b0ef8199
Balamurugan <murugan020325@gmail.com>
2026-05-16 19:36:09 +0530

    Update feature file path in TestNGRunner

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features = {
 
-                "src/test/resources/feature_files/darshan_raj/login.feature"
+                "src/test/resources/feature_files"
         },
 
         glue = {"stepDefinition", "hooks"},
