ebd1a315570f181a568336abb5f5cf55659205b0
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-18 15:49:32 +0530

    Update TestNGRunner for feature file paths and plugins

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -5,20 +5,21 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = {
-   "src\\test\\resources\\feature_files\\Janani_Sri\\Search_Patient.feature"
-//                "src\\test\\resources\\feature_files\\Janani_Sri\\Add_Patient.feature"
-        },
+    features = {
+        "src/test/resources/feature_files/Janani_Sri/Search_Patient.feature"
+    },
 
-        glue = {"stepDefinition"},
+    glue = {"stepDefinition"},
 
-        plugin = {
-                "pretty",
-                "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-            }
-        )
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber-report.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
+
+    monochrome = true
+)
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
