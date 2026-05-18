1852860c45f42ee9a3fd16123546c1614aa26ba4
Balamurugan <murugan020325@gmail.com>
2026-05-19 00:14:20 +0530

    added features files

---
+++ SmartHospitalTesting/src/test/java/stepDefinition/GeneratePharmacybillDefinition.java
@@ -0,0 +1,26 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GeneratePharmacybillDefinition {
+	@When("the user enters pharmacy bill details")
+	public void the_user_enters_pharmacy_bill_details(io.cucumber.datatable.DataTable dataTable) {
+	    
+	}
+
+	@When("user clicks the Save button")
+	public void user_clicks_the_save_button() {
+	    
+	}
+
+	@Then("the pharmacy bill should be generated successfully")
+	public void the_pharmacy_bill_should_be_generated_successfully() {
+	    
+	}
+
+	@Then("the error message should be displayed")
+	public void the_error_message_should_be_displayed() {
+	    
+	}
+}
