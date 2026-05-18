8c7e6d71cb0e2a93af3674e802993170366b9653
Harini R G <harinirg26@gmail.com>
2026-05-18 12:31:09 +0530

    The Logger file is added and implemented in DonorManagement feature.

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features = {
 
-                "src/test/resources/feature_files"
+                "src\\test\\resources\\feature_files"
         },
 
         glue = {"stepDefinition", "hooks"},
