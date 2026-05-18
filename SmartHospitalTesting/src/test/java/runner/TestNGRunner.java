c0efeb6b40c6144733b7454fd8331d24e7ad9019
Balamurugan <murugan020325@gmail.com>
2026-05-18 14:21:53 +0530

    Add DataProvider in TestNGRunner file

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -1,5 +1,5 @@
 package runner;
-
+import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -31,5 +31,9 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class TestNGRunner extends AbstractTestNGCucumberTests {
+    @DataProvider(parallel = false)
+public Object[][] scenarios() {
+    return super.scenarios();
+}
 
 }
