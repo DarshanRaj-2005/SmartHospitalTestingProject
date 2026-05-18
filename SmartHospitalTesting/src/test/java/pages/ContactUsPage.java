3b71d4d17688a9731ae6e73137bdb045e38b6dde
Tamilarasu143 <tamilarasu143@gmail.com>
2026-05-18 11:06:10 +0530

    missed features add

---
+++ SmartHospitalTesting/src/test/java/pages/ContactUsPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+
+
+import org.openqa.selenium.By;
+
+public class ContactUsPage {
+
+    public static By contactUsBtn = By.xpath("//a[normalize-space()='Contact Us']");
+
+    public static By name = By.xpath("//input[@id='name']");
+    public static By email = By.xpath("//input[@id='email']");
+    public static By subject = By.xpath("//input[@id='subject']");
+    public static By description = By.xpath("//textarea[@id='description']");
+
+    public static By submit = By.xpath("//input[@name='submit']");
+    
+    public static By successMessage =By.xpath("//div[@class='alert alert-success']");
+}
