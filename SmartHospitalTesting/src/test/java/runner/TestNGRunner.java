c86650e4a45ec919711ac502b48b4681caac34fb
Balamurugan <murugan020325@gmail.com>
2026-05-19 07:18:27 +0530

    Added pharmacy bill automation

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = { "src/test/resources/feature_files" },
+@CucumberOptions(features = { "src/test/resources/feature_files/GeneratePharmacybill.feature" },
 
 		glue = { "stepDefinition", "hooks" },
 
