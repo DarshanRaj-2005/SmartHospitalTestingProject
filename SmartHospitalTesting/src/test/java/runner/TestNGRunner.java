176db30c4551af10bd332b0d93ea4c6038e48846
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-19 17:31:54 +0530

    Update TestNGRunner.java

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/feature_files/Search_Patient.feature",
+        features = "src/test/resources/feature_files",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
@@ -15,11 +15,13 @@ import io.cucumber.testng.CucumberOptions;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
+        
+//        tags = "@ValidCredentials",
         monochrome = true
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios() {
         return super.scenarios();
     }
