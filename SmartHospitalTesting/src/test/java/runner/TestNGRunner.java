68fe0e8b2655ddbf52176b10c2874e23a6e0efcc
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-19 17:24:05 +0530

    Update TestNGRunner.java

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/feature_files",
+        features = "src/test/resources/feature_files/Search_Patient.feature",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
