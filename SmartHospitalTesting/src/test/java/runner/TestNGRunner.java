7b0ca5403895ce85558abaa5f3e999d1fc4cf18a
harinirg <harinirg26@gmail.com>
2026-05-19 17:12:50 +0530

    Update TestNGRunner.java

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/feature_files",
-        tag="@DonorManagement or @BloodIssueManagement or @BloodStockValidation",
+        tags="@DonorManagement or @BloodIssueManagement or @BloodStockValidation",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
