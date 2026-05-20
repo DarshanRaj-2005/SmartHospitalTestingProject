bb2d604d2643fcfb40d810724b403a62dbc1aef6
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-20 10:17:24 +0530

    Fix feature file path in TestNGRunner

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,8 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = //"src/test/resources/feature_files",
-    "src/test/resources/feature_files/Search_Patient.feature",
+        features = "src/test/resources/feature_files",
 
        
         tags="@Jananisri or @SearchPatient",
