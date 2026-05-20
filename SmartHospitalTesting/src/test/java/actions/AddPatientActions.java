385b219bd832ea89e6969c8e2bf448671c6f9ec9
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-20 16:08:17 +0530

    Update AddPatientActions.java

--- SmartHospitalTesting/src/test/java/actions/AddPatientActions.java
+++ SmartHospitalTesting/src/test/java/actions/AddPatientActions.java
@@ -12,7 +12,7 @@ import Utilities.Helper;
 import driver.Driver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-public class AddPatientAction {
+public class AddPatientActions {
    static Logger logger = LogManager.getLogger(AddPatientActions.class);
     private String lastPatientName = "";
 
