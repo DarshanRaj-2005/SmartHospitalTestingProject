711ab8ef36f1e63207571fbe1c01deb4139ec159
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-20 13:10:33 +0530

    Refactor patient name search methods for clarity

--- SmartHospitalTesting/src/test/java/actions/SearchPatientActions.java
+++ SmartHospitalTesting/src/test/java/actions/SearchPatientActions.java
@@ -20,19 +20,16 @@ public class SearchPatientActions {
         searchPatientPage.waitForPatientListToLoad();
     }
 
-    // Old step: "User enters patient name in search box"
     public void enterValidPatientName() {
         lastSearchedName = "John Marshall";
         searchPatientPage.enterSearchText(lastSearchedName);
     }
 
-    // Old step: "User enters invalid patient name"
     public void enterInvalidPatientName() {
         lastSearchedName = "XYZ123InvalidPatient";
         searchPatientPage.enterSearchText(lastSearchedName);
     }
 
-    // New Scenario Outline step: "User searches for patient name {string}"
     public void searchByName(String patientName) {
         lastSearchedName = patientName;
         searchPatientPage.enterSearchText(patientName);
