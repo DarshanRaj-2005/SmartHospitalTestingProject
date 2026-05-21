1ce8760b4fda28c21c7938a9ccd85e68d48d05bc
Darshan Raj J <dharshujayapal@gmail.com>
2026-05-21 16:08:51 +0530

    Change feature files path in TestNGRunner
    Updated feature files path to include all features.

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = {"src/test/resources/feature_files/AddAmbulance.feature", "src/test/resources/feature_files/login.feature"},
+        features = {"src/test/resources/feature_files},
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
