f82e332f2d7dc73c48c4a64482930138e622994c
Balamurugan <murugan020325@gmail.com>
2026-05-22 08:27:34 +0530

    Apply stash backup before cleanup

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -8,7 +8,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
         features = "src/test/resources/feature_files",
-
         glue = { "stepDefinition", "hooks" },
 		tags=("@bala"),
         plugin = {
@@ -16,7 +15,8 @@ import io.cucumber.testng.CucumberOptions;
                 "html:target/cucumber-report.html",
                 "json:target/cucumber-report.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failedrerun.txt"
         },
         
         monochrome = true
