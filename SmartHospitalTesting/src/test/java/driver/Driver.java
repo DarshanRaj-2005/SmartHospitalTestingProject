a823c6974074c6c14693e2291cab1586556d717e
tgtamilarasuk83 <tgtamilarasuk83@gmail.com>
2026-05-22 14:45:30 +0530

    Update Driver.java

--- SmartHospitalTesting/src/test/java/driver/Driver.java
+++ SmartHospitalTesting/src/test/java/driver/Driver.java
@@ -17,7 +17,7 @@ public class Driver {
     public static WebDriver getDriver() {
         return driver.get();
     }
-   /* public Driver() {
+    public Driver() {
 
         String browser = ConfigReader.getProperties().getProperty("browser");
         String headlessValue = ConfigReader.getProperties().getProperty("headless");
@@ -61,38 +61,8 @@ public class Driver {
         if (!headless) {
             getDriver().manage().window().maximize();
         }
-    }*/
-    public Driver() {
-
-        String browser = ConfigReader.getProperties().getProperty("browser");
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications");
-            options.setExperimentalOption("prefs", Map.of(
-                    "credentials_enable_service", false,
-                    "profile.password_manager_enabled", false));
-
-            driver.set(new ChromeDriver(options));   // ✅ IMPORTANT
-        }
-
-        else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-
-            FirefoxOptions options = new FirefoxOptions();
-
-            driver.set(new FirefoxDriver(options));  // ✅ IMPORTANT
-        }
-
-        else {
-            throw new RuntimeException("Invalid Browser Name: " + browser);
-        }
-
-        getDriver().manage().window().maximize();   // ✅ since no headless
     }
-    public static void quitDriver() {
+        public static void quitDriver() {
         if (getDriver() != null) {
             getDriver().quit();
             driver.remove();
