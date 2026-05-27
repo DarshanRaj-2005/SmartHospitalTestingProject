814a680cfc4a6deb422a67696437d252345421d0
Darshan Raj J <dharshujayapal@gmail.com>
2026-05-27 10:27:47 +0530

    Remove tag from Cucumber options in TestNGRunner
    Removed the tag '@Darshanraj' from Cucumber options.

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/feature_files",
-        tags = "@Darshanraj",
 
         glue = { "stepDefinition", "hooks" },
         plugin = {
