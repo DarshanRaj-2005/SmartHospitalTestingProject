9cb0967a6304f37dd439694139a25f910c3db248
Darshan Raj J <dharshujayapal@gmail.com>
2026-05-28 16:32:04 +0530

    Change Cucumber tag from '@ValidCredentials' to '@Darshanraj'

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		
 		features = "src/test/resources/feature_files",
-        tags = "@ValidCredentials",
+        tags = "@Darshanraj",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
