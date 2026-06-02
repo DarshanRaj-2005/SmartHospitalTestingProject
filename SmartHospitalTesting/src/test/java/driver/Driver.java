aed6925ab96cbe1adb0c2a1847dd84f25659cc96
Balamurugan <murugan020325@gmail.com>
2026-06-02 15:45:05 +0530

    Fix indentation in quitDriver method

--- SmartHospitalTesting/src/test/java/driver/Driver.java
+++ SmartHospitalTesting/src/test/java/driver/Driver.java
@@ -87,7 +87,7 @@ public class Driver {
     }
 
         public static void quitDriver() {
-        if (getDriver() != null) {
+            if (getDriver() != null) {
             getDriver().quit();
             driver.remove();
         }
