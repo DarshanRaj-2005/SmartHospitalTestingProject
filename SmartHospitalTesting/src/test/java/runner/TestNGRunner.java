8c78c2cec2ed573433fd336014bccaed21880b3f
harinirg <harinirg26@gmail.com>
2026-06-02 16:21:11 +0530

    Update TestNGRunner.java

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -9,6 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 
         features = "src/test/resources/feature_files",
         glue = { "stepDefinition", "hooks" },
+        tags="@Harini",
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html",
