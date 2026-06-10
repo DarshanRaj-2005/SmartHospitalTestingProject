21b6aa4962e1282cde26271832f13847649b2406
Balamurugan <murugan020325@gmail.com>
2026-06-11 00:36:28 +0530

    Add tags option to TestNGRunner for Cucumber tests

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = "src/test/resources/feature_files",
+		tags= "@bala",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
