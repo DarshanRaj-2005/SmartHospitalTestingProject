73298629a1e9c5eb78c0451189600f79c7a0ed66
Balamurugan <murugan020325@gmail.com>
2026-06-12 12:00:34 +0530

    Updated runner file

--- SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
+++ SmartHospitalTesting/src/test/java/runner/TestNGRunner.java
@@ -7,7 +7,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = "src/test/resources/feature_files",
-		tags= "@bala",
         glue = { "stepDefinition", "hooks" },
         plugin = {
                 "pretty",
