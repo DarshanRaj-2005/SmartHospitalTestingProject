70eb685bb426421a4bc33f10b7d9204608bd6b77
Balamurugan <murugan020325@gmail.com>
2026-08-07 14:20:38 +0530

    Update dashboard locator in LoginPage

--- SmartHospitalTesting/src/test/java/pages/LoginPage.java
+++ SmartHospitalTesting/src/test/java/pages/LoginPage.java
@@ -8,6 +8,6 @@ public class LoginPage {
 	public static By email = By.id("email");
 	public static By text = By.xpath("/html/body/div[1]/div/div/div/div[1]/div/div[2]/div[1]");
 	public static By texts = By.xpath("/html/body/div[1]/div/div/div/div[1]/div/div[2]/form/div[1]/span/p");
-	public static By dashboard = By.xpath("//*[@id=\"alert\"]/nav/div[1]/span");
+	public static By dashboard = By.xpath("//h1[normalize-space()='Dashboard']");
 	public static By password = By.id("password");
 }
